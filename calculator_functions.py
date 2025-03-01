94b2c84d8f38e84a85802710735c790813812e59
serasmecpt024 <serasmecpt024@student.wethinkcode.co.za>
2025-03-01 18:34:41 +0200

    performed arithmetic functions

--- calculator_functions.py
+++ calculator_functions.py
@@ -1,19 +1,27 @@
+import math
+
 def add(a, b):
     """
     Add two numbers and return the result.
     """
+    results= sum(a,b)
+    return results
     pass
 
 def subtract(a, b):
     """
     Subtract the second number from the first and return the result.
     """
+    results= a-b
+    return results
     pass
 
 def multiply(a, b):
     """
     Multiply two numbers and return the result.
     """
+    results = a* b
+    return results
     pass
 
 def divide(a, b):
@@ -21,6 +29,11 @@ def divide(a, b):
     Divide the first number by the second and return the result.
     Handle division by zero by returning "Error: Division by zero".
     """
+    
+    if b == 0:
+        return "Error: Division by zero"
+    results= a /b
+    return results
     pass
 
 def sqrt(a):
@@ -28,6 +41,11 @@ def sqrt(a):
     Return the square root of a number.
     Handle negative inputs by returning "Error: Negative input".
     """
+    if a < 0:
+        return "Error: Negative input"
+    else:
+        return math.sqrt(a)
+    
     pass
 
 def modulus(a, b):
@@ -35,10 +53,14 @@ def modulus(a, b):
     Return the remainder of the division of the first number by the second.
     Handle division by zero by returning "Error: Division by zero".
     """
-    pass
+    if b == 0:
+        return "Error: Division by zero"
+    return a % b
 
 def exponent(a, b):
     """
     Return the result of raising the first number to the power of the second.
     """
+    results = a **b
+    return results
     pass
